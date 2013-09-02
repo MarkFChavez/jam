8a735bade239246185ac96b54ae6973eeb2c501a
Mark Chavez <los_populares@yahoo.com>
2013-09-03 01:24:35 +0800

    for validation

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
+  	@justine = "tine.salangsang@gmail.com"
+  	@axel = "axelfelipe1224@gmail.com"
+  	@mark = "los_populares@yahoo.com"
   end
 end
